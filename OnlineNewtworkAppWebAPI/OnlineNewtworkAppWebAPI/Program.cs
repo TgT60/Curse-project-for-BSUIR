dd0de0d2d2c883b26ffeb0af71c4c5971823457f
TgT60 <dima.icigo@yandex.ru>
2023-11-07 00:04:00 +0300

    Create basic jwt for Admin

--- OnlineNewtworkAppWebAPI/OnlineNewtworkAppWebAPI/Program.cs
+++ OnlineNewtworkAppWebAPI/OnlineNewtworkAppWebAPI/Program.cs
@@ -1,4 +1,8 @@
+using System.Security;
+using System.Security.AccessControl;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => { 
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey 
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddAuthentication().AddJwtBearer();
 
 var app = builder.Build();
